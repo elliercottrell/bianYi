a79f46046729af2ac19f73616a6b3da93f776e3a
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-07-05 10:44:22 +0100

    added toLowerCase() for auto mobile entry

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ let explain = {
 };
 
 function translator() {
-  let search = document.querySelector("#search").value;
+  let search = document.querySelector("#search").value.toLowerCase();
   let h1 = document.querySelector("h1");
   h1.innerHTML = dictionary[search];
   let p = document.querySelector("p");
